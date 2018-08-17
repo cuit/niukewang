a25521aa30aa0b663ec22a4e96415a639a77d58b
shichao.xia <shichao.xia@bianlifeng.com>
2018-08-17 11:32:40 +0800

    fix

--- src/main/java/com/xsc/test/D_20180817/Test1.java
+++ src/main/java/com/xsc/test/D_20180817/Test1.java
@@ -20,17 +20,17 @@ public class Test1 {
             String str1 = scanner.nextLine();
             Iterable<String> iterable = Splitter.on(' ').split(str1);
             List<Integer> list = Lists.newArrayList();
-            for (String s: iterable){
+            for (String s : iterable) {
                 list.add(Integer.parseInt(s));
             }
             Integer k = scanner.nextInt();
             List<Integer> sortList = list.stream().sorted(new Comparator<Integer>() {
                 @Override
                 public int compare(Integer o1, Integer o2) {
-                    return o2-o1;
+                    return o2 - o1;
                 }
             }).collect(Collectors.toList());
-            System.out.println(sortList.get(k-1));
+            System.out.println(sortList.get(k - 1));
         }
     }
 }
